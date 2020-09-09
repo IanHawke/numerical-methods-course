d41895bdd8521ebd4ca3dd3fd2c582428a58b634
Ian Hawke <I.Hawke@soton.ac.uk>
2020-09-09 11:36:03 +0100

    Fix test.

--- exercises/test_02_07.py
+++ exercises/test_02_07.py
@@ -5,8 +5,8 @@ def test():
     # in the meta.json for details.
 
     # If an assertion fails, the message will be displayed
-    assert "import numpy as np", "Have you imported numpy in its short form?"
-    assert "np.sqrt", "Have you used the square root function from numpy?"
+    assert "import numpy as np" in __solution__, "Have you imported numpy in its short form?"
+    assert "np.sqrt" in __solution__, "Have you used the square root function from numpy?"
     assert abs(integrand_1(0) - 1) < 1e-10, "The function, called on 0, does not return 1?"
     assert abs(integrand_1(0.5) - 0.8660254037844386) < 1e-10, "The function, called on a random real number (less than 1!), does not return the correct value?"
 
